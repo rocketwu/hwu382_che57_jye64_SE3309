4311edd620157fcf2ed0c5ce183979619bccf4aa
hwu382@desktop <hwu382@uwo.ca>
2018-11-29 01:36:34 -0500

    finish delete function [new feature, response has code, 0 means fail, 1 means success]

--- assignment4/src/backend/server.js
+++ assignment4/src/backend/server.js
@@ -17,45 +17,108 @@ app.use(function (request, response, next) {
     next();
 });
 
+var c = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'itssql',
+    database: 'itssql'
+});
+
+c.connect(function (err) {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log('success connected to database');
+});
+
+c.query('SELECT * FROM recipedetail', function (err, res, field) {// TODO: 数据库连接测试，待删
+    console.log(res[5].recipeID);
+
+})
+
 router.route('/shoppinglist')
     .get(function (req, res) {
         //request to get shopping list
-        res.json({msg:'request to get shopping list', rec: req});
+        res.json({code: 0, msg:'request to get shopping list', rec: req.toString()});
     });
 
 router.route('/recommend')
     .get(function (req, res) {
         //request to get recommend recipe
-        res.json({msg:'request to get recommend recipe', rec: req});
+        res.json({code: 0, msg:'request to get recommend recipe', rec: req});
     });
 
 router.route('/inventory/:invID')
     .get(function (req, res) {
         //request to get recipe based on invID
-        res.json({msg: 'request to get recipe based on invID', rec: req});
+        res.json({code: 0, msg: 'request to get recipe based on invID', rec: req});
     });
 
 router.route('/recipe/:recID')
     .delete(function (req, res) {
         //request to delete recipe
-        res.json({msg: 'request to delete recipe', rec: req});
+        //res.json({msg: 'request to delete recipe', rec: req});
+        let recId = req.params.recID;
+        //check whether recipe is in a wish list
+        c.query(
+            'SELECT COUNT(*) AS num FROM `wisheddish` WHERE recipeID = ?',
+            [recId],
+            function (err, result, field) {
+                if (err){
+                    console.log(err);
+                    return;
+                } 
+                if (result[0].num == 0){
+                    //no wish list based on this recipe
+                    c.query(
+                        'DELETE FROM `recipedetail`' +
+                        'WHERE recipeID = ?',
+                        [recId],
+                        function (err, result, field) {
+                            if (err){
+                                console.log(err);
+                                return;
+                            }
+                            console.log('deleted ' + result.affectedRows + ' rows');
+                            c.query(
+                                'DELETE FROM `recipe`' +
+                                'WHERE recipeID = ?',
+                                [recId],
+                                function (err, result, field) {
+                                    if (err) {
+                                        console.log(err);
+                                        return;
+                                    }
+                                    console.log('deleted ' + result.affectedRows + ' rows');
+                                    res.json({code: 1, msg: 'Success deleted'});
+                                }
+                            )
+                        }
+                    )
+                }
+                else {
+                    res.json({code: 0, msg: 'Cannot delete this recipe because some of your wished dish need this recipe.'})
+                }
+            }
+        );
     });
 
 router.route('/inventory')
     .get(function (req, res) {
         //request to show all inventory
-        res.json({msg: 'request to show all inventory', rec: req});
+        res.json({code: 0, msg: 'request to show all inventory', rec: req});
     });
 
 router.route('/wisheddish')
     .get(function (req, res) {
         //request to show all wished dish
-        res.json({msg: 'request to show all wished dish', rec: req});
+        res.json({code: 0, msg: 'request to show all wished dish', rec: req});
     })
     
     .post(function (req, res) {
         //request to add wished dish
-        res.json({msg: 'request to add wished dish', rec: req});
+        res.json({code: 0, msg: 'request to add wished dish', rec: req});
     });
 
 
@@ -63,3 +126,6 @@ app.use('/', router);
 
 app.listen(port);
 console.log('Backend Server Up, Port: '+port);
+
+
+
