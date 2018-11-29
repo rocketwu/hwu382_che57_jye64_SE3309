566089a30f7efbac01d91fcf9a104fb889862ccd
hwu382@laptop <hwu382@uwo.ca>
2018-11-29 12:35:49 -0500

    finsh simple function

--- assignment4/src/backend/server.js
+++ assignment4/src/backend/server.js
@@ -18,7 +18,8 @@ app.use(function (request, response, next) {
 });
 
 var c = mysql.createConnection({
-    host: 'localhost',
+    host: '99.249.196.234',
+    port: 50102,
     user: 'root',
     password: 'itssql',
     database: 'itssql'
@@ -112,14 +113,14 @@ router.route('/inventory')
         //request to show all inventory
         // res.json({code: 0, msg: 'request to show all inventory', rec: req});
         c.query(
-            'SELECT * FROM inventorywithname',
+            'SELECT * FROM `inventorywithname`',
             function (err, result, field) {
                 if (err) {
                     console.log(err);
                     res.json({code: 2, msg: 'Unknown error'});
                     return;
                 }
-                res.json(result);
+                res.json({code: 1, msg: 'success', result: result});
             }
         )
     });
@@ -127,12 +128,63 @@ router.route('/inventory')
 router.route('/wisheddish')
     .get(function (req, res) {
         //request to show all wished dish
-        res.json({code: 0, msg: 'request to show all wished dish'});
+        //res.json({code: 0, msg: 'request to show all wished dish'});
+        c.query(
+            'SELECT * FROM `wisheddishwithname`',
+            function (err, result, field) {
+                if (err){
+                    console.log(err);
+                    res.json({code: 2, msg: 'Unknown error'});
+                    return;
+                }
+                res.json({code: 1, msg: 'success', result: result});
+            }
+        )
+        
     })
     
     .post(function (req, res) {
         //request to add wished dish
-        res.json({code: 0, msg: 'request to add wished dish'});
+        //body{dishName, dateToEat, quantity, memberID, recipeID}
+        let dishName = req.body.dishName,
+            dateToEat = req.body.dateToEat,
+            quantity = req.body.quantity,
+            memberID = req.body.memberID,
+            calorie,
+            recipeID = req.body.recipeID;
+        c.query(
+            //find calorie of recipe first
+            'SELECT calorie FROM `recipe` WHERE recipeID = ?',
+            [recipeID],
+            function (error, result, field) {
+                if (error){
+                    console.log(err);
+                    res.json({code: 2, msg: 'Unknown error'});
+                    return;
+                }
+                if (result.length == 0){
+                    res.json({code: 0, msg: 'The recipe you choose is not exist anymore!'});
+                }else {
+                    //find the recipe, calculate calorie
+                    calorie = quantity * result[0].calorie;
+                    console.log(calorie);
+                    c.query(
+                        'INSERT INTO `wisheddish` (`dishName`, `dateToEat`, `quantity`, `calorie`, `dateOfAdding`, `memberID`, `recipeID`)' +
+                        'VALUES (?, ?, ?, ?, ?, ?, ?)',
+                        [dishName, dateToEat, quantity, calorie, mysql.raw('CURDATE()'), memberID, recipeID],
+                        function (err, result, field) {
+                            if (error){
+                                console.log(err);
+                                res.json({code: 2, msg: 'Unknown error'});
+                                return;
+                            }
+                            res.json({code:1, msg: 'Add wished dish success!'});
+                        }
+                    )
+                }
+
+            }
+        )
     });
 
 
